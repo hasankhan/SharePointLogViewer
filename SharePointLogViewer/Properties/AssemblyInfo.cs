de53fde65f7888218de0a37c070807aee7406c07
hasankhan_cp <SND\hasankhan_cp>
2010-01-24 05:27:22 -0800

    added application icon

--- SharePointLogViewer/Properties/AssemblyInfo.cs
+++ SharePointLogViewer/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("SharePointLogViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("diyatech")]
+[assembly: AssemblyCompany("Overroot")]
 [assembly: AssemblyProduct("SharePointLogViewer")]
-[assembly: AssemblyCopyright("Copyright © diyatech 2010")]
+[assembly: AssemblyCopyright("Copyright © Overroot 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
