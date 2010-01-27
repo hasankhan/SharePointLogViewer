3dff9fc45b84d65604edfc4cc609bd7e5683aaa0
hasankhan_cp <SND\hasankhan_cp>
2010-01-27 10:51:35 -0800

    removed reference to sharepoint dll finding path of sharepoint and logs directory using registry information.

--- SharePointLogViewer/SPUtility.cs
+++ SharePointLogViewer/SPUtility.cs
@@ -2,24 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.SharePoint.Administration;
 using System.IO;
+using Microsoft.Win32;
+using System.Security;
 
 namespace SharePointLogViewer
 {
     class SPUtility
     {
-        public static string GetLogsLocations()
+        public static bool IsWSSInstalled
         {
-            try
+            get
             {
-                SPDiagnosticsService service = new SPDiagnosticsService("log", SPWebService.ContentService.Farm);
-                return service.LogLocation;
+                try
+                {
+                    RegistryKey key = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Microsoft\Shared Tools\Web Server Extensions\12.0");
+                    if (key != null)
+                    {
+                        object val = key.GetValue("SharePoint");
+                        if (val != null && val.Equals("Installed"))
+                            return true;
+                    }
+                }
+                catch (SecurityException) { }
+                return false;
             }
-            catch (Exception)
+        }
+
+        public static bool IsMOSSInstalled
+        {
+            get
             {
-                return String.Empty;
+                try
+                {
+                    using (RegistryKey key = GetSharePointRegistryKey())
+                        if (key != null)
+                        {
+                            string versionStr = key.GetValue("BuildVersion") as string;
+                            if (versionStr != null)
+                            {
+                                Version buildVersion = new Version(versionStr);
+                                if (buildVersion.Major == 12)
+                                    return true;
+                            }
+                        }
+                }
+                catch (SecurityException) {}
+                return false;
             }
+        }        
+
+        public static string LogsLocations
+        {
+            get 
+            {
+                string logsPath = SharePointInstallPath;
+                if (logsPath != String.Empty)
+                    logsPath = Path.Combine(logsPath, "logs");
+
+                return logsPath;
+            }
+        }
+
+        public static string SharePointInstallPath
+        {
+            get
+            {
+                string installPath = String.Empty;
+                try
+                {
+                    using (RegistryKey key = GetSharePointRegistryKey())
+                        if (key != null)
+                            installPath = key.GetValue("InstallPath").ToString();
+                }
+                catch (SecurityException) { }
+                return installPath;
+            }
+        }
+
+        static RegistryKey GetSharePointRegistryKey()
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Office Server\12.0");
+            return key;
         }
     }
 }
