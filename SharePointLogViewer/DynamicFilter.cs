d972e5ee267bc4554e263ef509520ead12736fe1
hasankhan_cp <SND\hasankhan_cp>
2010-03-18 11:45:54 -0700

    made searching efficient

--- SharePointLogViewer/DynamicFilter.cs
+++ SharePointLogViewer/DynamicFilter.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 namespace SharePointLogViewer
 {
     class DynamicFilter
-    {
+    {        
         Predicate<object> predicate;
 
         private DynamicFilter(Predicate<object> predicate)
@@ -30,26 +30,51 @@ namespace SharePointLogViewer
             else if (propertyName == "*")
             {
                 var predicates = from property in typeof(T).GetProperties()
-                                 select CreatePredicate<T>(property.Name, searchText);
+                                 select CreatePredicate(property, searchText);
                 predicate = entry => predicates.Any(p => p(entry));
             }
             else
-                predicate = delegate(object entry)
-                {
-                    PropertyInfo property = typeof(T).GetProperty(propertyName);
-                    if (property == null)
-                        return false;
-                    string value = property.GetValue(entry, null) as String;
-                    if (value == null)
-                        return false;
-                    return value.ToLower().Contains(searchText);
-                }; ;
+                predicate = CreatePredicate(typeof(T).GetProperty(propertyName), searchText);
+                                        
             return predicate;
         }
 
         public bool IsMatch(object item)
         {
             return predicate(item);
-        }        
+        }
+
+        static Predicate<object> CreatePredicate(PropertyInfo property, string searchText)
+        {
+            var comparer = new PropertyValueComparer(property, searchText);
+            return new Predicate<object>(comparer.IsMatch);
+        }
+
+        #region Predicate
+
+        class PropertyValueComparer
+        {
+            PropertyInfo property;
+            string searchText;
+
+            public PropertyValueComparer(PropertyInfo property, string searchText)
+            {
+                this.property = property;
+                this.searchText = searchText;
+            }
+
+            public PropertyValueComparer(Type type, string propertyName, string searchText)
+                : this(type.GetProperty(propertyName), searchText) { }
+
+            public bool IsMatch(object entry)
+            {
+                object value = property.GetValue(entry, null);
+                if (value == null)
+                    return false;
+                return value.ToString().ToLower().Contains(searchText);
+            }
+        } 
+
+        #endregion
     }
 }
