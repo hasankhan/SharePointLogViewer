7e9005f8e845502a559823e90bfb1d811c1e9c38
hasankhan_cp <SND\hasankhan_cp>
2010-03-22 12:48:50 -0700

    trim the log entry fields because they have redundant spaces appended to them

--- SharePointLogViewer/LogEntry.cs
+++ SharePointLogViewer/LogEntry.cs
@@ -18,18 +18,18 @@ namespace SharePointLogViewer
 
         public static LogEntry Parse(string line)
         {
-            string[] fields = line.Split(new char[] { '\t' });
+            string[] fields = line.Split('\t');
             var entry = new LogEntry()
             {
-                Timestamp = fields[0],
-                Process = fields[1],
-                TID = fields[2],
-                Area = fields[3],
-                Category = fields[4],
-                EventID = fields[5],
-                Level = fields[6],
-                Message = fields[7],
-                Correlation = fields[8]
+                Timestamp = fields[0].Trim(),
+                Process = fields[1].Trim(),
+                TID = fields[2].Trim(),
+                Area = fields[3].Trim(),
+                Category = fields[4].Trim(),
+                EventID = fields[5].Trim(),
+                Level = fields[6].Trim(),
+                Message = fields[7].Trim(),
+                Correlation = fields[8].Trim()
             };
             return entry;
         }
